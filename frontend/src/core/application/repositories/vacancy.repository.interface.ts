bdbbdf9b12ede4599fef09988e54e71647c2172f
slpng <mottomottohora@gmail.com>
2024-11-22 05:36:03 +0300

    refactor: change get vacancy repo return value

--- frontend/src/core/application/repositories/vacancy.repository.interface.ts
+++ frontend/src/core/application/repositories/vacancy.repository.interface.ts
@@ -4,6 +4,6 @@ export interface IVacancyRepository {
     createVacancy: (vacancy: VacancyInsert) => Promise<Vacancy>;
     removeVacancy: (id: string) => Promise<void>;
     editVacancy: (id: string, vacancy: VacancyInsert) => Promise<Vacancy>;
-    getVacancy: (id: string) => Promise<Vacancy | undefined>;
+    getVacancy: (id: string) => Promise<Vacancy>;
     getAllVacancies: () => Promise<Vacancy[]>;
 }
