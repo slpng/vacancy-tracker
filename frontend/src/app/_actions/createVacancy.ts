5c29f2df1eaa13d65adba20e666c5e0b171bfad4
slpng <mottomottohora@gmail.com>
2024-11-22 05:28:00 +0300

    refactor: remove unused import

--- frontend/src/app/_actions/createVacancy.ts
+++ frontend/src/app/_actions/createVacancy.ts
@@ -2,7 +2,6 @@
 
 import { repository } from "@/app/_repository";
 import { InputParseError } from "@/core/entities/errors/common";
-import { VacancyRepositoryInMemory } from "@/core/infrastructure/vacancy.repository.in-memory";
 import { createVacancyController } from "@/core/interface-adapters/controllers/vacancy/create-vacancy.controller";
 import { revalidatePath } from "next/cache";
 
