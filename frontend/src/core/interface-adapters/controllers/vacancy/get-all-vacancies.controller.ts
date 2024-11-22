4e8aa7a12e370a3b8f772e575b01a7d52afe46b1
slpng <mottomottohora@gmail.com>
2024-11-22 05:33:05 +0300

    feat: implement get all vacancies controller

---
+++ frontend/src/core/interface-adapters/controllers/vacancy/get-all-vacancies.controller.ts
@@ -0,0 +1,18 @@
+import { IVacancyRepository } from "@/core/application/repositories/vacancy.repository.interface";
+import { getAllVacanciesUseCase } from "@/core/application/use-cases/vacancy/get-all-vacancies.use-case";
+import { Vacancy } from "@/core/entities/models/vacancy";
+import { vacancyPresenter } from "@/core/interface-adapters/presenters/vacancy";
+
+const presenter = (vacancies: Vacancy[]) => {
+    return vacancies.map((vacancy) => vacancyPresenter(vacancy));
+};
+
+export const getAllVacanciesController = (repository: IVacancyRepository) => {
+    const useCase = getAllVacanciesUseCase(repository);
+
+    return async () => {
+        const vacancies = await useCase();
+
+        return presenter(vacancies);
+    };
+};
