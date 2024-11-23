fe8ec071c20952d7ff039ebbfdfa3936d6ceff4a
slpng <mottomottohora@gmail.com>
2024-11-23 04:59:05 +0300

    refactor: implement edit vacancy controller and move insert vacancy validation to utils

--- frontend/src/core/interface-adapters/controllers/vacancy/create-vacancy.controller.ts
+++ frontend/src/core/interface-adapters/controllers/vacancy/create-vacancy.controller.ts
@@ -1,41 +1,13 @@
-import { z } from "zod";
-
 import { IVacancyRepository } from "@/core/application/repositories/vacancy.repository.interface";
 import { createVacancyUseCase } from "@/core/application/use-cases/vacancy/create-vacancy.use-case";
-import { insertVacancySchema } from "@/core/entities/models/vacancy";
-import { InputParseError } from "@/core/entities/errors/common";
 import { vacancyPresenter } from "@/core/interface-adapters/presenters/vacancy";
-
-const inputSchema = z.object({
-    company: z.string().min(1),
-    position: z.string().min(1),
-    minSalary: z.string().min(1),
-    maxSalary: z.string().min(1),
-    status: z.string().min(1),
-    note: z.string(),
-});
+import { validateVacancyInsert } from "@/core/interface-adapters/utils";
 
 export const createVacancyController = (repository: IVacancyRepository) => {
     const useCase = createVacancyUseCase(repository);
 
     return async (input: object) => {
-        const { data: inputData, error: inputError } =
-            inputSchema.safeParse(input);
-
-        if (inputError) {
-            throw new InputParseError("Invalid input data", {
-                cause: inputError,
-            });
-        }
-
-        const { data: insertData, error: insertError } =
-            insertVacancySchema.safeParse(inputData);
-
-        if (insertError) {
-            throw new InputParseError("Invalid insert data", {
-                cause: insertError,
-            });
-        }
+        const { insertData } = validateVacancyInsert(input);
 
         const created = await useCase(insertData);
 
---
+++ frontend/src/core/interface-adapters/utils.ts
@@ -0,0 +1,39 @@
+import { InputParseError } from "@/core/entities/errors/common";
+import {
+    insertVacancySchema,
+    VacancyInsert,
+} from "@/core/entities/models/vacancy";
+import { z } from "zod";
+
+const inputSchema = z.object({
+    id: z.string(),
+    company: z.string().min(1),
+    position: z.string().min(1),
+    minSalary: z.string().min(1),
+    maxSalary: z.string().min(1),
+    status: z.string().min(1),
+    note: z.string(),
+});
+
+export const validateVacancyInsert = (
+    input: object
+): { id: string; insertData: VacancyInsert } => {
+    const { data: inputData, error: inputError } = inputSchema.safeParse(input);
+
+    if (inputError) {
+        throw new InputParseError("Invalid input data", {
+            cause: inputError,
+        });
+    }
+
+    const { data: insertData, error: insertError } =
+        insertVacancySchema.safeParse(inputData);
+
+    if (insertError) {
+        throw new InputParseError("Invalid insert data", {
+            cause: insertError,
+        });
+    }
+
+    return { id: inputData.id, insertData };
+};
