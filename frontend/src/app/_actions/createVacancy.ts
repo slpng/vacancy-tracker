6735175e9197e051bb6f3c4a250af31bb356765b
slpng <mottomottohora@gmail.com>
2024-11-23 05:09:15 +0300

    feat: implement vacancy edit

--- frontend/src/app/_actions/createVacancy.ts
+++ frontend/src/app/_actions/createVacancy.ts
@@ -1,17 +1,31 @@
 "use server";
 
+import { ModalType } from "@/app/_contexts/ModalContext";
 import { repository } from "@/app/_repository";
 import { InputParseError } from "@/core/entities/errors/common";
 import { createVacancyController } from "@/core/interface-adapters/controllers/vacancy/create-vacancy.controller";
+import { editVacancyController } from "@/core/interface-adapters/controllers/vacancy/edit-vacancy.controller";
 import { revalidatePath } from "next/cache";
 
-const controller = createVacancyController(repository);
+export type ActionState = { success: boolean; message: string };
 
 export const createVacancy = async (
-    prevState: { message: string },
-    formData: FormData
-) => {
-    const data = Object.fromEntries(formData.entries());
+    prevState: ActionState,
+    payload: { formData: FormData; modalType: ModalType }
+): Promise<ActionState> => {
+    const data = Object.fromEntries(payload.formData.entries());
+    let controller;
+
+    if (payload.modalType === "create") {
+        controller = createVacancyController(repository);
+    } else if (payload.modalType === "edit") {
+        controller = editVacancyController(repository);
+    } else {
+        return {
+            success: false,
+            message: "Invalid modal type",
+        };
+    }
 
     try {
         const created = await controller(data);
@@ -19,11 +33,13 @@ export const createVacancy = async (
         revalidatePath("/", "page");
 
         return {
+            success: true,
             message: JSON.stringify(created),
         };
     } catch (err) {
         if (err instanceof InputParseError) {
             return {
+                success: false,
                 message: JSON.stringify({
                     message: err.message,
                     cause: err.cause,
@@ -31,6 +47,7 @@ export const createVacancy = async (
             };
         }
         return {
+            success: false,
             message: "Unknown error",
         };
     }
