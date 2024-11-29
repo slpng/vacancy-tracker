6f53e8b9e76f15a1519544edbb0d29eeece1753d
slpng <mottomottohora@gmail.com>
2024-11-29 04:09:21 +0300

    fix: fix custom error prototypes

--- core/entities/errors/common.ts
+++ core/entities/errors/common.ts
@@ -1,11 +1,13 @@
 export class NotFoundError extends Error {
     constructor(message: string, options?: ErrorOptions) {
         super(message, options);
+        Object.setPrototypeOf(this, new.target.prototype);
     }
 }
 
 export class InputParseError extends Error {
     constructor(message: string, options?: ErrorOptions) {
         super(message, options);
+        Object.setPrototypeOf(this, new.target.prototype);
     }
 }
